624088f1591879d9cb988b6287d1042b52e4f2c8
Navdeep Singh <navi3776@gmail.com>
2018-11-19 23:56:07 +0530

    Adds Repositories

---
+++ src/main/java/com/shopping_cart/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shopping_cart.repositories;
+
+import com.shopping_cart.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
