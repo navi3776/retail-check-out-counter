b506dba5c9656a09037d16f5194c046d3d7eea6e
Navdeep Singh <navi3776@gmail.com>
2018-11-19 23:53:54 +0530

    Adds a rest controller

---
+++ src/main/java/com/shopping_cart/controller/OrderCheckOutController.java
@@ -0,0 +1,30 @@
+package com.shopping_cart.controller;
+
+import com.shopping_cart.domain.BillDetails;
+import com.shopping_cart.domain.CheckOutOrderDetails;
+import com.shopping_cart.service.CreateOrderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OrderCheckOutController {
+
+    Logger logger = LogManager.getLogger(OrderCheckOutController.class);
+
+    @Autowired
+    private CreateOrderService createOrderService;
+
+    @RequestMapping(value = "/checkOutOrder/", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+    @ResponseBody
+    public BillDetails checkOutOrder(@RequestBody CheckOutOrderDetails orderDetails) {
+        logger.info(String.format(" Entering OrderCheckOutController for the current order with order details [%s]",orderDetails));
+
+        BillDetails billDetails = createOrderService.createOrder(orderDetails);
+        logger.info(String.format(" Exiting the OrderCheckOutController with order status as [%s]",billDetails .getReturnMessage()));
+        return billDetails;
+    }
+
+
+}
