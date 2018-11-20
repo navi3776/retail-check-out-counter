91abb728057f4877ac3f0866003872d12b96c78b
Navdeep Singh <navi3776@gmail.com>
2018-11-20 23:44:17 +0530

    Major Refactoring

--- src/main/java/com/shopping_cart/repositories/CategoryRepository.java
+++ src/main/java/com/shopping_cart/repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.shopping_cart.repositories;
 
-import com.shopping_cart.domain.Category;
+import com.shopping_cart.domain.TaxCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<TaxCategory,Long> {
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into category (category_Id,name,tax_Value) values(101,'Category A',10);
-insert into category (category_Id,name,tax_Value) values(102,'Category B',20);
-insert into category (category_Id,name,tax_Value) values(103,'Category C',0);
+insert into tax_category (category_Id,name,value) values(101,'Category A',10);
+insert into tax_category (category_Id,name,value) values(102,'Category B',20);
+insert into tax_category (category_Id,name,value) values(103,'Category C',0);
 
 insert into product (product_Id,product_Name,price,units_in_inventory,description,manufacturer)
 values (1001,'Samsung S8',40000.50,5,'Samsung Mobile Phone','Samsung');
@@ -12,4 +12,3 @@ values (1003,'SkullCandy Earphones',1600.99,80,'SkullCAndy Earphones White','Sku
 insert into product_category_xref (product_Category_Id,product_Id,category_Id) values(101,1001,102);
 insert into product_category_xref (product_Category_Id,product_Id,category_Id) values(102,1003,103);
 insert into product_category_xref (product_Category_Id,product_Id,category_Id) values(103,1002,101);
-
